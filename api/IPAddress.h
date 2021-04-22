d6eeee4691b90b77a3f7e606e5392011a310d728
Ahti Legonkov <ahti.legonkov@gmail.com>
2021-04-22 15:08:19 +0300

    Reduce amount of friends

--- api/IPAddress.h
+++ api/IPAddress.h
@@ -63,7 +63,7 @@ public:
     bool operator==(const uint8_t* addr) const;
 
     // Overloaded index operator to allow getting and setting individual octets of the address
-    uint8_t operator[](int index) const { return _address.bytes[index]; };
+    uint8_t const& operator[](int index) const { return _address.bytes[index]; };
     uint8_t& operator[](int index) { return _address.bytes[index]; };
 
     // Overloaded copy operators to allow initialisation of IPAddress objects from other types
@@ -71,14 +71,6 @@ public:
     IPAddress& operator=(uint32_t address);
 
     virtual size_t printTo(Print& p) const;
-
-    friend class UDP;
-    friend class Client;
-    friend class Server;
-    
-    friend ::EthernetClass;
-    friend ::DhcpClass;
-    friend ::DNSClient;
 };
 
 extern const IPAddress INADDR_NONE;
